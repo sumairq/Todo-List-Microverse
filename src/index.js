17daa595683aa77418cdf93a5422a0dcdfdc9296
Sumair <sumairqaisar992@gmail.com>
2022-01-27 22:12:41 +0500

    fix linting errors

--- src/index.js
+++ src/index.js
@@ -3,39 +3,38 @@ import './style.css';
 const todoItems = [
 
   {
-    description: "Walk the Dog",
-    completed: "false",
-    index: 0
+    description: 'Walk the Dog',
+    completed: 'false',
+    index: 0,
   },
   {
-    description: "Iron the shirt",
-    completed: "false",
-    index: 1
+    description: 'Iron the shirt',
+    completed: 'false',
+    index: 1,
   },
   {
-    description: "Attend meeting",
-    completed: "false",
-    index: 2
-  }
-]
+    description: 'Attend meeting',
+    completed: 'false',
+    index: 2,
+  },
+];
 
-const todoList = document.querySelector(".todo-list")
+const todoList = document.querySelector('.todo-list');
 
 const renderTodoList = (items) => {
-
-  for(let i = 0; i < todoItems.length; i += 1 ) {
-      let todoItem = document.createElement('div');
-      todoItem.classList.add("flex-item")
-      todoItem.innerHTML = `<div class="flex-container">
+  for (let i = 0; i < todoItems.length; i += 1) {
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('flex-item');
+    todoItem.innerHTML = `<div class="flex-container">
       <div class="input">
       <input type="checkbox" name="${items[i].description}" id="${items[i].description}">
       <label for="${items[i].description}">${items[i].description}</label>
       </div>
       <i class="fas fa-ellipsis-v"></i>
       </div>`;
-      todoList.appendChild(todoItem)
-      console.log(items[i])
-    }
+    todoList.appendChild(todoItem);
+    console.log(items[i]);
   }
+};
 
-document.addEventListener('DOMContentLoaded', renderTodoList(todoItems))
+document.addEventListener('DOMContentLoaded', renderTodoList(todoItems));
