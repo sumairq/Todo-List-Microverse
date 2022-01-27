b2e73bd86b6ebf33b203193e0611827165bb4c4e
Sumair <sumairqaisar992@gmail.com>
2022-01-27 21:46:45 +0500

    Add to do list structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
+const todoItems = [
+
+  {
+    description: "Walk the Dog",
+    completed: "false",
+    index: 0
+  },
+  {
+    description: "Iron the shirt",
+    completed: "false",
+    index: 1
+  },
+  {
+    description: "Attend meeting",
+    completed: "false",
+    index: 2
   }
-  
-  document.body.appendChild(component());
+]
+
+const todoList = document.querySelector(".todo-list")
+
+const renderTodoList = (items) => {
+
+  for(let i = 0; i < todoItems.length; i += 1 ) {
+      let todoItem = document.createElement('div');
+      todoItem.classList.add("flex-item")
+      todoItem.innerHTML = `<div class="flex-container">
+      <div class="input">
+      <input type="checkbox" name="${items[i].description}" id="${items[i].description}">
+      <label for="${items[i].description}">${items[i].description}</label>
+      </div>
+      <i class="fas fa-ellipsis-v"></i>
+      </div>`;
+      todoList.appendChild(todoItem)
+      console.log(items[i])
+    }
+  }
+
+document.addEventListener('DOMContentLoaded', renderTodoList(todoItems))
