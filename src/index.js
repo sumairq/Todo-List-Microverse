080970817cfd8bbf31f573b73054800e61d27197
Sumair <sumairqaisar992@gmail.com>
2022-01-29 02:04:41 +0500

    Fix linting errors

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import TodoCollection from './modules/todoCollection';
-import Item from './modules/item';
+import TodoCollection from './modules/todoCollection.js';
+import Item from './modules/item.js';
 import './style.css';
 
-const tasksCol = new TodoCollection()
+const tasksCol = new TodoCollection();
 const input = document.querySelector('.task-input');
 
-if(localStorage.getItem('tasksList')){
-  const storageItems = JSON.parse(localStorage.getItem('tasksList')).tasksList
-  storageItems.forEach(task => {
-
-    tasksCol.create(new Item(task.index, task.description, task.completed))
-    
-  })
+if (localStorage.getItem('tasksList')) {
+  const storageItems = JSON.parse(localStorage.getItem('tasksList')).tasksList;
+  storageItems.forEach((task) => {
+    tasksCol.create(new Item(task.index, task.description, task.completed));
+  });
 }
 
 window.addEventListener('keydown', (e) => {
-  if(e.keyCode === 13 && input.value !== ''){
-    tasksCol.create(new Item(tasksCol.list.length, input.value, false))
-    input.value = ''
+  if (e.keyCode === 13 && input.value !== '') {
+    tasksCol.create(new Item(tasksCol.list.length, input.value, false));
+    input.value = '';
   }
-})
+});
