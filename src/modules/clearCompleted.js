38a0da84d4f2529f09fa00929b461330659b658b
Sumair <sumairqaisar992@gmail.com>
2022-01-29 16:13:22 +0500

    Make final changes

--- src/modules/clearCompleted.js
+++ src/modules/clearCompleted.js
@@ -8,7 +8,6 @@ export default function clear(list) {
     }
   });
   for (let i = indexes.length - 1; i >= 0; i -= 1) {
-      console.log(todoList.children[indexes[i]].children[0].children[1])
     if (todoList.children[indexes[i]].children[0].children[1]) {
         todoList.children[indexes[i]].children[0].children[1].click();
     }
